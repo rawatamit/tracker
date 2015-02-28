e114ae50e1301ba25967609b85699ba16873ca19
arjunlalb <arjunlal.b@gmail.com>
2015-02-28 18:11:00 +0530

    3 pages, transit working

--- TagAlong/TagAlong.WindowsPhone/CreatePickSource.xaml.cs
+++ TagAlong/TagAlong.WindowsPhone/CreatePickSource.xaml.cs
@@ -13,19 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace TagAlong
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class CreatePickSource : Page
     {
-        public MainPage()
+        public CreatePickSource()
         {
             this.InitializeComponent();
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
@@ -35,19 +34,15 @@ namespace TagAlong
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
         }
 
-        private void Button_Click_CreateTag(object sender, RoutedEventArgs e)
+        private void Click_GetCurrLoc(object sender, RoutedEventArgs e)
         {
-            
-            this.Frame.Navigate(typeof(CreatePickSource));
+
+        }
+
+        private void Click_GetUsingMaps(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
